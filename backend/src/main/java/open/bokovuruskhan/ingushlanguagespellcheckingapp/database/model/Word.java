534314ea4c78b98546401f68731bf8d8178e68b6
Uruskhan Bokov <bokovuruskhan@gmail.com>
2023-11-13 01:13:33 +0300

    working prototype (levenshtein)

--- backend/src/main/java/open/bokovuruskhan/ingushlanguagespellcheckingapp/database/model/Word.java
+++ backend/src/main/java/open/bokovuruskhan/ingushlanguagespellcheckingapp/database/model/Word.java
@@ -15,6 +15,6 @@ public class Word {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true,nullable = false)
     private String word;
 }
