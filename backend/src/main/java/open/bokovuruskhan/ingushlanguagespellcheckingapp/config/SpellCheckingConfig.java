53ef266a5b61ad0461adbaed6f6e7838fee0715b
Uruskhan Bokov <bokovuruskhan@gmail.com>
2023-11-21 04:29:16 +0300

    moved

--- backend/src/main/java/open/bokovuruskhan/ingushlanguagespellcheckingapp/config/SpellCheckingConfig.java
+++
@@ -1,7 +0,0 @@
-package open.bokovuruskhan.ingushlanguagespellcheckingapp.config;
-
-public class SpellCheckingConfig {
-
-    public static final int LEVENSHTEIN_MAX_DISTANCE = 4;
-
-}
--- backend/src/main/java/open/bokovuruskhan/ingushlanguagespellcheckingapp/database/repository/WordRepository.java
+++
@@ -1,9 +0,0 @@
-package open.bokovuruskhan.ingushlanguagespellcheckingapp.database.repository;
-
-import open.bokovuruskhan.ingushlanguagespellcheckingapp.database.model.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WordRepository extends JpaRepository<Word, Long> {
-}
